7d0f79697f682c306ddceb7a89ccec811109a68d
Muhammad Husain <Muhammad Husain>
2016-12-07 16:03:50 +0700

    finishing template

--- assets/js/main1.js
+++ assets/js/main1.js
@@ -2,6 +2,7 @@ var i = 1;
 $(document).ready(function() {
 	$('#section1').find('h5').first().hide();
 	$('#section1').find('p').first().hide();
+	$('#section1').find('img').first().hide();
 	$('#section0').find('p').first().hide();
 	$('#navigation-pane').hide();
 	$('#section0').find('h5').first().hide();
@@ -15,22 +16,23 @@ $(document).ready(function() {
         	if(index == 2){
 				$('#section1').find('h5').first().fadeIn(1000);
 				$('#section1').find('p').first().fadeIn(1000);
+				$('#section1').find('img').first().fadeIn(1000);
         	}
         	if(index == 1){
-        		$('#section0').fadeIn(1000, function () {
-        			$('#navigation-pane').fadeIn(1800, function () {
-	        			$('#section0').find('p').first().fadeIn(1000);
-	        			$('#section0').find('h5').first().fadeIn(1000);
-	        			$('#section0').find('button').last().fadeIn(1000);
+        		$('#section0').fadeIn(100, function () {
+        			$('#navigation-pane').fadeIn(100, function () {
+	        			$('#section0').find('p').first().fadeIn(100);
+	        			$('#section0').find('h5').first().fadeIn(100);
+	        			$('#section0').find('button').last().fadeIn(100);
 	        			$('#animation-container').show();
 	        			var picture1 = document.getElementById('picture1');
 						var myVar = setInterval(myTimer, 100);
 						function myTimer() {
 						    picture1.src = 'assets/img/img1/smartphone_000'+ i +'.png'
 						    i++;
-						    if (i > 29) {
+						    if (i > 24) {
 						    	myStopFunction();
-						    	i = 30;
+						    	i = 25;
 						    }
 						}
 
@@ -44,4 +46,9 @@ $(document).ready(function() {
         }
       });
       $('.slider').slider({full_width: true});
+       $('.carousel.carousel-slider').carousel({full_width: true});
+       var myVar2 = setInterval(myTimer2, 3000);
+		function myTimer2() {
+       		$('.carousel.carousel-slider').carousel('next');
+   		}
     });
