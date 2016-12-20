4fa303fc1b4ac3640f811751af62f6cb5833b5f6
Muhammad Husain <Muhammad Husain>
2016-12-20 15:08:45 +0700

    changes of feature pages

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,21 +1,54 @@
-// var lastScrollTop = 0;
-// $(window).scroll(function(event){
-//    var st = $(this).scrollTop();
-//     if (st > lastScrollTop){
-//         $('html, body').animate({
-//         	scrollTop: $("#full3").offset().top
-//     	}, 2000);
-//     	lastScrollTop = $("#full3").offset().top;
-//     } else {
-//        $('html, body').animate({
-//         	scrollTop: $("#full2").offset().top
-//     	}, 2000);
-//        lastScrollTop = $("#full2").offset().top;
-//     }
-//     console.log(st + ">" + lastScrollTop);
+var i = 1;
+$(document).ready(function() {
+	$('#section1').find('h5').first().hide();
+	$('#section1').find('p').first().hide();
+	$('#section1').find('img').first().hide();
+	$('#section0').find('p').first().hide();
+	$('#navigation-pane').hide();
+	$('#section0').find('h5').first().hide();
+    $('#section0').find('button').last().hide();
+    $('#animation-container').hide();
+    $('#section0').hide();
+      $('#fullpage').fullpage({
+        verticalCentered: false,
+        css3:false,
+        afterLoad: function(anchorLink, index){
+        	if(index == 2){
+				$('#section1').find('h5').first().fadeIn(1000);
+				$('#section1').find('p').first().fadeIn(1000);
+				$('#section1').find('img').first().fadeIn(1000);
+        	}
+        	if(index == 1){
+        		$('#section0').fadeIn(100, function () {
+        			$('#navigation-pane').fadeIn(100, function () {
+	        			$('#section0').find('p').first().fadeIn(100);
+	        			$('#section0').find('h5').first().fadeIn(100);
+	        			$('#section0').find('button').last().fadeIn(100);
+	        			$('#animation-container').show();
+	        			var picture1 = document.getElementById('picture1');
+						var myVar = setInterval(myTimer, 100);
+						function myTimer() {
+						    picture1.src = 'assets/img/img1/smartphone_000'+ i +'.png'
+						    i++;
+						    if (i > 24) {
+						    	myStopFunction();
+						    	i = 25;
+						    }
+						}
 
-// });
-$(document).ready(function(){
-      $('.carousel.carousel-slider').carousel({full_width: true});
+						function myStopFunction() {
+						    clearInterval(myVar);
+						}
+        			});
+        		});
+
+        	}
+        }
+      });
+      $('.slider').slider({full_width: true});
+       $('.carousel.carousel-slider').carousel({full_width: true});
+       var myVar2 = setInterval(myTimer2, 3000);
+		function myTimer2() {
+       		$('.carousel.carousel-slider').carousel('next');
+   		}
     });
-
